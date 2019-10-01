9b7a8cce0c7b9161b8c5405c0bb585234360423e
Olivier Giroux <ogiroux@gmail.com>
2019-10-01 09:25:57 -0700

    This should have worked

--- src/barrier.cpp
+++ src/barrier.cpp
@@ -13,6 +13,7 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 #if !defined(_LIBCPP_HAS_NO_TREE_BARRIER) && !defined(_LIBCPP_HAS_NO_THREAD_FAVORITE_HASH)
 
+_LIBCPP_EXPORTED_FROM_ABI
 thread_local ptrdiff_t __libcpp_thread_favorite_hash = 0;
 //    hash<__thread_id>()(this_thread::get_id());
 
